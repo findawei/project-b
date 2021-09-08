4ea790561b55135120082234298553b8bd841e20
Alex Benjamignan <hayhoky@gmail.com>
2021-09-08 14:10:07 -0400

    v1 - added carousel

--- client/src/components/Auctions.js
+++ client/src/components/Auctions.js
@@ -1,60 +1,61 @@
-import React , { useEffect } from 'react';
-import {connect} from 'react-redux';
-import { makeStyles } from '@material-ui/core/styles';
-import {Grid, Paper, Container } from '@material-ui/core/'
-import {getItems} from '../flux/actions/itemActions'
-import Listing from './Listing'
-import {isFuture, isPast} from "date-fns";
-import FileUpload from './FileUpload';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Paper, Container } from "@material-ui/core/";
+import { getItems } from "../flux/actions/itemActions";
+import Listing from "./Listing";
+import { isFuture, isPast } from "date-fns";
+import FileUpload from "./FileUpload";
 
-const Auctions = ({getItems, item}) => {
-
-
-const useStyles = makeStyles((theme) => ({
+const Auctions = ({ getItems, item }) => {
+  const useStyles = makeStyles((theme) => ({
     root: {
-        flexGrow: 1,
-        margin: 10,
-        paddingTop: 10
+      flexGrow: 1,
+      margin: 10,
+      paddingTop: 10,
     },
-}));
+  }));
 
-const classes = useStyles();
+  const classes = useStyles();
 
-  useEffect(() => { 
-    getItems(); 
+  useEffect(() => {
+    getItems();
   }, [getItems]);
- 
+
   const { items } = item;
 
   return (
     <div className={classes.root}>
-    <Container> 
-    <Grid container spacing={2}>
-      {items.filter(opt => isFuture(new Date(opt.endDate))).map(item => <Listing 
-          item={item} 
-          key={item._id}/>
-          )}
-    </Grid>
-    {items.filter(opt => isPast(new Date(opt.endDate))).length === 0 ?
-    ''
-    :
-    <div>
-    <h1>Ended</h1>
-    <Grid container spacing={2}>
-      {items.filter(opt => isPast(new Date(opt.endDate))).sort((a, b) => (new Date(b.endDate)) - (new Date(a.endDate))).map(item => <Listing 
-          item={item} 
-          key={item._id}/>
-          )}
-    </Grid>
-    </div>
-    }
-    </Container>
+      <Container>
+        <Grid container spacing={2}>
+          {items
+            .filter((opt) => isFuture(new Date(opt.endDate)))
+            .map((item) => (
+              <Listing item={item} key={item._id} />
+            ))}
+        </Grid>
+        {items.filter((opt) => isPast(new Date(opt.endDate))).length === 0 ? (
+          ""
+        ) : (
+          <div>
+            <h1>Ended</h1>
+            <Grid container spacing={2}>
+              {items
+                .filter((opt) => isPast(new Date(opt.endDate)))
+                .sort((a, b) => new Date(b.endDate) - new Date(a.endDate))
+                .map((item) => (
+                  <Listing item={item} key={item._id} />
+                ))}
+            </Grid>
+          </div>
+        )}
+      </Container>
     </div>
   );
-}
+};
 
 const mapStateToProps = (state) => ({
-  item: state.item
+  item: state.item,
   // auth: state.auth
 });
 
--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import {Grid} from '@material-ui/core/';
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core/";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -9,35 +11,35 @@ const useStyles = makeStyles((theme) => ({
     // justifyContent: 'space-around',
     // overflow: 'hidden',
     // backgroundColor: theme.palette.background.paper
-    flexGrow: 1
-    },
-    thumbnail: {
-      height: 50,
-    },
-    hero: {
-        borderRadius: 3,
-        minHeight: 300,
-        maxHeight:650,
-        width:"100%",
-        height: "100%",
-        objectFit: 'cover'
-    }
+    flexGrow: 1,
+  },
+  // thumbnail: {
+  //   height: 50,
+  // },
+  hero: {
+    borderRadius: 3,
+    minHeight: 300,
+    maxHeight: 650,
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+  },
 }));
 
-const Gallery = ({img}) => {
+const Gallery = ({ img }) => {
   const classes = useStyles();
 
-const [clickedImg, setClickedImg] = useState()
+  // const [clickedImg, setClickedImg] = useState()
 
-useEffect(()=>{
-  if(img && img[0]){
-    setClickedImg(img[0].url)
-  }
-},[img])
+  // useEffect(()=>{
+  //   if(img && img[0]){
+  //     setClickedImg(img[0].url)
+  //   }
+  // },[img])
 
   return (
     <div className={classes.root}>
-      <img src={clickedImg} className={classes.hero}/>
+      {/* <img src={clickedImg} className={classes.hero}/>
       <Grid 
        container
        direction="row"
@@ -54,9 +56,22 @@ useEffect(()=>{
               />
           </Grid>
         ))}
-      </Grid>
+      </Grid> */}
+      <Carousel infiniteLoop emulateTouch useKeyboardArrows>
+        {img &&
+          img.map((tile) => (
+            <div key={tile.id}>
+              <img
+                src={tile.url}
+                alt={tile.title}
+                // className={classes.thumbnail}
+                // onClick={()=>setClickedImg(tile.url)}
+              />
+            </div>
+          ))}
+      </Carousel>
     </div>
   );
-}
+};
 
 export default Gallery;
